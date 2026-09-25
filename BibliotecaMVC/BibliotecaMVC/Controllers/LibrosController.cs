5952cfaa1f4e9b4a08f486c0e119b315b6462532
CarlosR <ElMar@XCARLOS7>
2026-09-24 22:54:57 -0600

    Implementacion de CRUD de Libros con EF Core (Editar y Eliminar) e interfaz UI/UX.

--- BibliotecaMVC/BibliotecaMVC/Controllers/LibrosController.cs
+++ BibliotecaMVC/BibliotecaMVC/Controllers/LibrosController.cs
@@ -35,5 +35,54 @@ namespace BibliotecaMVC.Controllers
             }
             return View(libro);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var libro = _context.Libros.Find(id);
+            if (libro == null) return NotFound();
+
+            return View(libro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, Libro libro)
+        {
+            if (id != libro.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Libros.Update(libro);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(libro);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            // Uso del método Find() según la indicación
+            var libro = _context.Libros.Find(id);
+            if (libro == null) return NotFound();
+
+            return View(libro);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var libro = _context.Libros.Find(id);
+            if (libro != null)
+            {
+                _context.Libros.Remove(libro);
+                _context.SaveChanges();
+            }
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
